cf0f271ec99887af33d0903365c4904a53b6207e
DavideCruciani <cruciani240203@gmail.com>
2024-01-11 08:09:38 +0100

    storing data in a separate file

---
+++ src/assets/data/faqData.js
@@ -0,0 +1,30 @@
+export const faqData = [
+  {
+    title: "What is Frontend Mentor, and how will it help me?",
+    text: `Frontend Mentor offers realistic coding challenges to help
+              developers improve their frontend coding skills with projects in
+              HTML, CSS and JavaScript, it's suitable for all levels and ideal
+              for portfolio building.`,
+  },
+  {
+    title: "Is Frontend Mentor free?",
+    text: `Yes, Frontend Mentor offers realistic coding challenges to help
+              developers improve their frontend coding skills with projects in
+              HTML, CSS and JavaScript, it's suitable for all levels and ideal
+              for portfolio building.`,
+  },
+  {
+    title: "Can i use Frontend Mentor projects in my portfolio?",
+    text: `Yes, you can Frontend Mentor offers realistic coding challenges to help
+              developers improve their frontend coding skills with projects in
+              HTML, CSS and JavaScript, it's suitable for all levels and ideal
+              for portfolio building.`,
+  },
+  {
+    title: "How can i get help if i'm stuck on a challenge?",
+    text: `Frontend Mentor offers realistic coding challenges to help
+              developers improve their frontend coding skills with projects in
+              HTML, CSS and JavaScript, it's suitable for all levels and ideal
+              for portfolio building.`,
+  },
+];
--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,4 +1,5 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
+import { faqData } from "../assets/data/faqData";
 
 const Faq = ({ starIcon, plusIcon, minusIcon }) => {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -15,134 +16,40 @@ const Faq = ({ starIcon, plusIcon, minusIcon }) => {
       </div>
       <div>
         <ul>
-          <li className="py-4 border-b-2 border-[#f9f0ff] relative bg-white">
-            <div className="flex items-center justify-between gap-5">
-              <h2
-                className="font-bold text-[#2f1533] text-lg cursor-pointer hover:text-[#AD28EB]"
-                onClick={() => {
-                  if (isExpanded == 1) {
-                    expandIndex(!isExpanded);
-                  } else {
-                    expandIndex(1);
-                  }
-                }}
-              >
-                What is Frontend Mentor, and how will it help me?
-              </h2>
-              <img
-                src={isExpanded === 1 ? minusIcon : plusIcon}
-                alt="Minus Icon"
-              />
-            </div>
-            <p
-              className={
-                isExpanded == 1
-                  ? "text-[#8c6991] py-4 h-auto duration-200 ease-in-out"
-                  : "h-0 opacity-0 duration-200 overflow-hidden"
-              }
+          {faqData.map((card, index) => (
+            <li
+              className="py-4 border-b-2 border-[#f9f0ff] relative bg-white"
+              key={index}
             >
-              Frontend Mentor offers realistic coding challenges to help
-              developers improve their frontend coding skills with projects in
-              HTML, CSS and JavaScript, it's suitable for all levels and ideal
-              for portfolio building.
-            </p>
-          </li>
-          <li className="py-4 border-b-2 border-[#f9f0ff] relative bg-white">
-            <div className="flex items-center justify-between gap-5">
-              <h2
-                className="font-bold text-[#2f1533] text-lg cursor-pointer hover:text-[#AD28EB]"
-                onClick={() => {
-                  if (isExpanded == 2) {
-                    expandIndex(!isExpanded);
-                  } else {
-                    expandIndex(2);
-                  }
-                }}
+              <div className="flex items-center justify-between gap-5">
+                <h2
+                  className="font-bold text-[#2f1533] text-lg cursor-pointer hover:text-[#AD28EB]"
+                  onClick={() => {
+                    if (isExpanded === index) {
+                      expandIndex(!isExpanded);
+                    } else {
+                      expandIndex(index);
+                    }
+                  }}
+                >
+                  {card.title}
+                </h2>
+                <img
+                  src={isExpanded === index ? minusIcon : plusIcon}
+                  alt={isExpanded === index ? "Minus Icon" : "Plus Icon"}
+                />
+              </div>
+              <p
+                className={
+                  isExpanded === index
+                    ? "text-[#8c6991] py-4 h-auto duration-200 ease-in-out"
+                    : "h-0 opacity-0 duration-200 overflow-hidden"
+                }
               >
-                Is Frontend Mentor free?
-              </h2>
-              <img
-                src={isExpanded === 2 ? minusIcon : plusIcon}
-                alt="Plus Icon"
-              />
-            </div>
-            <p
-              className={
-                isExpanded == 2
-                  ? "text-[#8c6991] py-4 h-auto duration-200 ease-in-out"
-                  : "h-0 opacity-0 duration-200 overflow-hidden"
-              }
-            >
-              Frontend Mentor offers realistic coding challenges to help
-              developers improve their frontend coding skills with projects in
-              HTML, CSS and JavaScript, it's suitable for all levels and ideal
-              for portfolio building.
-            </p>
-          </li>
-          <li className="py-4 border-b-2 border-[#f9f0ff] relative bg-white">
-            <div className="flex items-center justify-between gap-5">
-              <h2
-                className="font-bold text-[#2f1533] text-lg cursor-pointer hover:text-[#AD28EB]"
-                onClick={() => {
-                  if (isExpanded == 3) {
-                    expandIndex(!isExpanded);
-                  } else {
-                    expandIndex(3);
-                  }
-                }}
-              >
-                Can i use Frontend Mentor projects in my portfolio?
-              </h2>
-              <img
-                src={isExpanded === 3 ? minusIcon : plusIcon}
-                alt="Plus Icon"
-              />
-            </div>
-            <p
-              className={
-                isExpanded == 3
-                  ? "text-[#8c6991] py-4 h-auto duration-200 ease-in-out"
-                  : "h-0 opacity-0 duration-200 overflow-hidden"
-              }
-            >
-              Frontend Mentor offers realistic coding challenges to help
-              developers improve their frontend coding skills with projects in
-              HTML, CSS and JavaScript, it's suitable for all levels and ideal
-              for portfolio building.
-            </p>
-          </li>
-          <li className="pt-4 relative bg-white">
-            <div className="flex items-center justify-between gap-5">
-              <h2
-                className="font-bold text-[#2f1533] text-lg cursor-pointer hover:text-[#AD28EB]"
-                onClick={() => {
-                  if (isExpanded == 4) {
-                    expandIndex(!isExpanded);
-                  } else {
-                    expandIndex(4);
-                  }
-                }}
-              >
-                How can i get help if i'm stuck on a challenge?
-              </h2>
-              <img
-                src={isExpanded === 4 ? minusIcon : plusIcon}
-                alt="Plus Icon"
-              />
-            </div>
-            <p
-              className={
-                isExpanded == 4
-                  ? "text-[#8c6991] py-4 h-auto duration-200 ease-in-out"
-                  : "h-0 opacity-0 duration-200 overflow-hidden"
-              }
-            >
-              Frontend Mentor offers realistic coding challenges to help
-              developers improve their frontend coding skills with projects in
-              HTML, CSS and JavaScript, it's suitable for all levels and ideal
-              for portfolio building.
-            </p>
-          </li>
+                {card.text}
+              </p>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
